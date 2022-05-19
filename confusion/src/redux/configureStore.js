bca111e320c667292289fe71fbb78fb4ee521882
MD DILSHAD ALAM <talkdilshad@gmail.com>
2022-05-19 15:54:10 +0530

    Combining Reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,11 +1,23 @@
 /* import {createStore} from 'redux'; */
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {combineReducers, createStore} from 'redux';
+// import { Reducer, initialState } from './reducer'
+import {Dishes} from './dishes';
+import {Comments} from './comments';
+import {Leaders} from './leaders';
+import {Promotions} from './promotions';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        // Reducer, // reducer
+        // initialState, // our initialState
+
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
--- confusion/src/redux/reducer.js
+++ confusion/src/redux/reducer.js
@@ -1,9 +1,6 @@
 /* this is where we will setup our state... */
 
-import {DISHES} from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import {PROMOTIONS} from '../shared/promotion';
-import {LEADERS} from '../shared/leaders';
+
 // javascript obj
 export const initialState = {
     dishes: DISHES,
