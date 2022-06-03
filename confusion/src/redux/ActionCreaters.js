2b585514c464832675be660180a55ef6f5c9114b
MD DILSHAD ALAM <talkdilshad@gmail.com>
2022-06-03 15:09:53 +0530

    fetch handling

--- confusion/src/redux/ActionCreaters.js
+++ confusion/src/redux/ActionCreaters.js
@@ -18,11 +18,29 @@ export const addComment = (dishId, rating , author, comment)=> ({
 export const fetchDishes = () => (dispatch)=> {
     dispatch(dishesLoading(true));
     return fetch(baseUrl+'dishes')
-        .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
-    // setTimeout(() =>  {
-    //     dispatch(addDishes(DISHES));
-    // },2000);
+    .then(response =>  {
+        if(response.ok){
+            return response;
+        }
+        else 
+        {
+            var error= new Error('Error ' + response.status + ': '+response.statusText);
+            error.response = response;
+            throw error;
+        }
+
+    
+    },
+    error => {
+        var error_message = new Error(error.message);
+        throw error_message;
+    })
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)));
+// setTimeout(() =>  {
+//     dispatch(addDishes(DISHES));
+// },2000);
 }
 
 export const dishesLoading = () => ({
@@ -41,8 +59,29 @@ export const addDishes = (dishes) => ({
 
 export const fetchComments = () => (dispatch) => {    
     return fetch(baseUrl + 'comments')
+    .then(response =>  {
+        if(response.ok){
+            return response;
+        }
+        else 
+        {
+            var error= new Error('Error' + response.status + ': '+response.statusText);
+            error.response = response;
+            throw error;
+        }
+
+    
+    },
+    error => {
+        var error_message = new Error(error.message);
+        throw error_message;
+    })
+
+
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
+
 };
 
 export const commentsFailed = (error_message) => ({
@@ -58,8 +97,27 @@ export const addComments = (comments)=> ({
 export const fetchPromos = () => (dispatch)=> {
     dispatch(promosLoading());
     return fetch(baseUrl+'promotions')
+    .then(response =>  {
+        if(response.ok){
+            return response;
+        }
+        else 
+        {
+            var error= new Error('Error' + response.status + ': '+response.statusText);
+            error.response = response;
+            throw error;
+        }
+
+    
+    },
+    error => {
+        var error_message = new Error(error.message);
+        throw error_message;
+    })
+
         .then(response => response.json())
-        .then(promos => dispatch(addPromos(promos)));
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
     
 }
 
